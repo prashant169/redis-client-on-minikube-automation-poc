f932b9f11e8799e359e66e57a1f7a5be72311229
prashant devops <prashant>
2025-07-15 17:40:54 +0530

    a

--- scripts/2_install_redis.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo update
-helm install redis bitnami/redis --set auth.enabled=false
-
--- scripts/5_get_key.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-POD_NAME=$(kubectl get pods -l app=redis-client -o jsonpath="{.items[0].metadata.name}")
-kubectl exec -it "$POD_NAME" -- redis-cli -h redis-master GET OxKey
-
